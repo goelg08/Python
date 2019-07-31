1eed739fbbeb017d5d312bc0d64142c34e700d7f
Priyanshu Goel <Priyanshu.Goel@factset.com>
2019-07-31 14:23:04 +0530

    Blog and Google python

---
+++ Google_Python/hello.py
@@ -0,0 +1,9 @@
+import sys
+
+#Define  a main() function which prints little greetings
+def main():
+	print("Hello!")
+	
+#This is standard boilerplate which calls the main method
+if __name__ == '__main__':
+	main()
